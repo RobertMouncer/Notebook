657242a17ce5600b9c9d67193e0e57107cbd1e75
rdm10 <rdm10@imac-b57-35.dcs.aber.ac.uk>
2019-04-06 16:15:17 +0100

    Initial Commit

---
+++ noteBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  noteBook
+//
+//  Created by rdm10 on 06/04/2019.
+//  Copyright © 2019 rdm10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
