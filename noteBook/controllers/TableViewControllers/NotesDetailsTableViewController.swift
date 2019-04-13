4ad36733d34171659c8ee0bbec57aa950cf84226
rdm10 <rdm10@imac-b57-35.dcs.aber.ac.uk>
2019-04-13 19:52:28 +0100

    ability to remove links from notes takn

--- noteBook/controllers/TableViewControllers/NotesDetailsTableViewController.swift
+++ noteBook/controllers/TableViewControllers/NotesDetailsTableViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
-
+import CoreData
 class NotesDetailsTableViewController: UITableViewController {
     
     var notesItem: Notes?
     var articleResult: GuardianOpenPlatformResult!
+     var managedContext: NSManagedObjectContext?
+    
     @IBOutlet weak var titleTextView: UITextView!
     
     @IBOutlet weak var notesTextView: UITextView!
@@ -19,6 +21,12 @@ class NotesDetailsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        guard let delegate = UIApplication.shared.delegate as? AppDelegate else {
+            print("error - unable to access failure")
+            exit(EXIT_FAILURE)
+        }
+        
+        managedContext = delegate.persistentContainer.viewContext
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -66,25 +74,32 @@ class NotesDetailsTableViewController: UITableViewController {
         }
     }
     
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            do {
+                notesItem?.removeFromArticles(notesItem?.articles?.object(at: indexPath.row) as! Article)
+                notesItem?.setValue(Date(), forKey: "lastModified")
+                try managedContext?.save()
+                print("deleted url")
+            }
+            catch let error as NSError {
+                print("error with \(error)")
+            }
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
